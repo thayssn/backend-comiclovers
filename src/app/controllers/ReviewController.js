3545a7ccc4a7d93a10592bf8b25aa3522cce8ef9
Thays <thays.sn@hotmail.com>
2019-08-10 13:25:56 -0300

    returning the review when posting one

--- src/app/controllers/ReviewController.js
+++ src/app/controllers/ReviewController.js
@@ -24,58 +24,21 @@ class ReviewController {
         },
       });
 
-      review.setBook(book);
-      review.setUser(req.userId);
+      await review.setBook(book);
+      await review.setUser(req.userId);
 
       if (!wasCreated) {
-        review.update({
+        await review.update({
           rating,
         });
       }
 
-      const reviews = await book.getReviews();
-
-      book.total_rating = reviews.reduce((f, n) => {
-        return f + n.rating;
-      }, 0);
-
-      const total = book.total_rating / reviews.length;
-      book.save();
-
       return res.status(201).json({
-        total,
-      });
-    } catch (err) {
-      console.log(err);
-      return res.status(400).json({ error: 'Error creating review' });
-    }
-  }
-
-  async update(req, res) {
-    const { rating } = req.body;
-    try {
-      const review = await Review.findOne({
-        where: {
-          book_id: req.params.id,
-          user_id: req.userId,
-        },
-      });
-
-      if (!review) {
-        return res.status(404).json({
-          error: 'Review not found',
-        });
-      }
-
-      await review.update({ rating });
-
-      return res.status(200).json({
-        success: true,
         review,
       });
     } catch (err) {
       console.log(err);
-      return res.status(400).json({ error: 'Error tring to update review' });
+      return res.status(400).json({ error: 'Error creating review' });
     }
   }
 }
