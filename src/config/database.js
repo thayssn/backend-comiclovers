8423534dac1e0aff000dc10908bce3e09c740b5f
Thays <thays.sn@hotmail.com>
2020-02-15 12:20:54 -0300

    FIX - Error database not found

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres',
-  host: 'db',
+  host: process.env.DB_HOST,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
